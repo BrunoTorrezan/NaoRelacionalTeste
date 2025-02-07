2e0eb77d88f7e86d4048acfb572bee91578d0015
BrunoTorrezan <125081424+BrunoTorrezan@users.noreply.github.com>
2025-02-07 13:20:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('Teste1.html', titulo="Testando Vercel")
+    return render_template('index.html', titulo="Testando Vercel")
 
 @app.route('/2')
 def hello_world2():
