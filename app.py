813c2cabe118ad5c532f5495ab8fa426f3cf9832
BrunoTorrezan <125081424+BrunoTorrezan@users.noreply.github.com>
2025-02-07 14:05:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request,redirect
+from tinydb import TinyDB
 
+db = TinyDB('banco.json')
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,11 +10,28 @@ def home():
 
 @app.route('/cadastro')
 def cadastro():
-    return render_template('Cadastro.html', titulo ="Cadastro Não Relacional" )
+    estados = [SP,GO]
+    return render_template('Cadastro.html', titulo ="Cadastro Não Relacional", estados = estados)
 
 @app.route('/exibir')
 def exibir():
     return render_template('Exibir.html', titulo ="Exibir")
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    telefone = request.form['telefone']
+    datanas = request.form['datanas']
+    genero = request.form.get('gender')
+    endereco = request.form['endereco']
+    estado = request.form['estado']
+    cidade = request.form['cidade']
+    Dado = {'nome':nome,'telefone':telefone,
+            'DataNascimento':datanas,'Genero':genero,
+            'Endereco':endereco,'Estado':estado,'Cidade':cidade}
+    db.insert(Dado)
+    return redirect('/exibir')
+
 
 
 if __name__ == '__main__':
